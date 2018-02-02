2654acd22b9d480f32699275ad4fba6edbecf1c0
Eldar Khalilov <eldar@oneclass.com>
2018-02-01 20:40:23 -0500

    init

---
+++ src/components/App/index.js
@@ -0,0 +1,4 @@
+import App from './App.jsx';
+
+export default App;
+
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'stylesheets/index.css';
+import App from 'components/App';
+import registerServiceWorker from 'helpers/registerServiceWorker';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
